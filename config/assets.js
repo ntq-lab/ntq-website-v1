03b9b8df9ccb8dce9849ac6f0e7e8d73739e6c5f
Luc Hoang Long <d@dapps.me>
2015-03-13 14:30:27 +0700

    fix assets.js

--- config/assets.js
+++ config/assets.js
@@ -1,18 +1,30 @@
 'use strict';
 
+var _ = require('lodash');
+var assetManager = require('assetmanager');
+var url = require('url');
+
 var debug = process.env.NODE_ENV === 'development';
 // var debug = false;
 var tempFolder = 'build/.tmp';
 var assets;
 
 if (debug) {
-	assets = require('./assets.json');
+	assets = assetManager.process({
+		assets: _.pick(require('./assets.json'), 'js', 'css'),
+		debug: true,
+		webroot: '/'
+	});
 
 	[assets.css, assets.js].forEach(function iterate(collection) {
 		Object.keys(collection).forEach(function iterate(key) {
 			var optKey = key.replace(tempFolder, '');
 
 			assets[optKey] = collection[key];
+
+			collection[key].forEach(function iterate(path, index) {
+				collection[key][index] = url.resolve('/', path);
+			});
 
 			delete collection[key];
 		});
@@ -28,7 +40,7 @@ if (debug) {
 	Object.keys(rev).forEach(function iterate(key) {
 		var optKey = key.replace(tempFolder, '');
 
-		assets[optKey] = [rev[key]];
+		assets[optKey] = [url.resolve('/', rev[key])];
 	});
 }
 
